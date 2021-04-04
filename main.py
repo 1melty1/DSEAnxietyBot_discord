17b6c4d95bb242d5547a9f2725df3241799b06ac
Wyw <53013464+1melty1@users.noreply.github.com>
2021-04-04 14:36:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import os
 from random import randrange
 from datetime import datetime, timedelta
 from refresh import keep_alive
-from PIL import Image, ImageFont, ImageDraw 
+from PIL import Image, ImageFont, ImageDraw, ImageFilter
 
 client = discord.Client()
+
+
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Game(name=' ?dse'))
 
 @client.event
 async def on_message(message):
@@ -20,6 +24,7 @@ async def on_message(message):
         #time
         dse_2022 = datetime(2022, 3, 31, 8, 30)
         time_now_hk = datetime.today() - timedelta(hours=16) #16 is the time diff between server and hk
+        print("Ping time: ", datetime.today())
         print(dse_2022 - time_now_hk)
         diff = (dse_2022 - time_now_hk).total_seconds()
         line1 = "Time left 2022 dse"
@@ -29,17 +34,18 @@ async def on_message(message):
         #2022 dse: 31/3/2022 8:30am
 
         #image processing
-        img_path = "background" + str(randrange(1)) + ".jpg"
+        img_path = "background" + str(randrange(2)) + ".jpg"
         print(img_path)
         img = Image.open(img_path)
         width, height = img.size
+        blurred_img = img.filter(ImageFilter.GaussianBlur(radius = 25))
         font = ImageFont.truetype('futura.ttf', width//10)
-        draw = ImageDraw.Draw(img)
+        draw = ImageDraw.Draw(blurred_img)
         draw.text((20, height//5), line1, (255,255,255),font=font)
         draw.text((20, height//5*2), line2, (255,255,255),font=font)
         draw.text((20, height//5*3), line3, (255,255,255),font=font)
         draw.text((20, height//5*4), line4, (255,255,255),font=font)
-        img.save('output.jpg')
+        blurred_img.save('output.jpg')
 
         #await message.channel.send(text)
 
